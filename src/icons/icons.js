aa22e96c54afcc792e55d90bd0d7f9be510ec1c6
Zarni Hpyoe <zarnihpyoe@gmail.com>
2017-05-09 19:44:19 +0630

    feat: Add some social media icons

---
+++ src/icons/icons.js
@@ -0,0 +1,7 @@
+import Email from '../icons/Email'
+import Github from '../icons/Github'
+import Medium from '../icons/Medium'
+import Twitter from '../icons/Twitter'
+import Linkedin from '../icons/Linkedin'
+
+export { Email, Github, Medium, Twitter, Linkedin }
